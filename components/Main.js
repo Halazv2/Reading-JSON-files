67547b19b8f52ef339a0fa363e5109353f246ee1
Halazv2 <halaziani2003@gmail.com>
2022-09-29 11:54:49 +0100

    home page

--- components/Main.js
+++ components/Main.js
@@ -75,37 +75,10 @@ export default function Main() {
           <img className="w-10" src="https://nine4.app/favicon.png"></img>
           <h3 class="pt-3 font-semibold text-lg text-white">LIVE</h3>
           <p class="pt-2 value-text text-md text-gray-200 fkrr1">
-            With Microsoft's Monaco Editor which is also used by VS Code, easily
-            edit your JSON and directly view through the graphs. Also there's a
-            JSON validator above of it to make sure there is no type error.
+           With Microsoft's Monaco Editor which is also used by VS Code, easily edit your JSON and directly view through the graphs. Also there's a JSON validator above of it to make sure there is no type error.
           </p>
         </div>
       </div>
-      <section class="relative pb-24">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 text-center">
-          <div class="py-24 md:py-36">
-            <h1 class="mb-5 text-6xl font-bold text-white">
-              Subscribe to our newsletter
-            </h1>
-            <h1 class="mb-9 text-2xl font-semibold text-gray-200">
-              Enter your email address and get our newsletters straight away.
-            </h1>
-            <input
-              type="email"
-              placeholder="jack@example.com"
-              name="email"
-              autoComplete="email"
-              class="border border-gray-600 w-1/4 pr-2 pl-2 py-3 mt-2 rounded-md text-gray-800 font-semibold hover:border-gray-700 bg-black"
-            />{" "}
-            <a
-              class="inline-flex items-center px-14 py-3 mt-2 ml-2 font-medium text-black transition duration-500 ease-in-out transform bg-transparent border rounded-lg bg-white"
-              href="/"
-            >
-              <span class="justify-center">Subscribe</span>
-            </a>
-          </div>
-        </div>
-      </section>
     </section>
   );
 }
