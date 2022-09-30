1928adfae79331d767af1741ad95ad84ddd9d232
Halazv2 <halaziani2003@gmail.com>
2022-09-30 11:37:31 +0100

    clean code + log an err if user does not exist

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const controllers = require("../controllers");
 const router = express.Router();
 
 router.route("/users").get(controllers.getAllusers).post(controllers.createuser);
-router.route("/login").post(controllers.loginuser);
+router.route("/login").post(controllers.login);
 router
  .route("/:id")
  .get(controllers.getuser)
