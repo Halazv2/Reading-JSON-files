1c8b56eae0c867dff56fdb5111bc1c52e4788439
Halazv2 <halaziani2003@gmail.com>
2022-10-01 14:43:15 +0100

    add codemirror

--- frontend/components/user/Main.js
+++ frontend/components/user/Main.js
@@ -1,42 +1,60 @@
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { useCallback, useState } from "react";
+import { json } from "@codemirror/lang-json";
+
 export default function Main() {
+  const onChange = useCallback((value, viewUpdate) => {
+    setJsoninput(value);
+  }, []);
+  const [jsoninput, setJsoninput] = useState("");
   return (
     <section className="text-gray-600 body-font">
-      <div className="max-w-5xl pt-36 pb-24 mx-auto">
+      <div className="max-w-5xl pt-36 pb-6 mx-auto">
         <h1 className="text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
           <span className="text-red-700">JSON</span> Map
         </h1>
       </div>
-
+      <div className="mb-5">
+        <h3 className="text-3xl font-bold text-center text-white">
+          please make sure your json is valid before you click on the map button
+        </h3>
+      </div>
       <div className="flex items-center justify-center w-1/2 mx-auto mb-20">
-        <label
-          for="dropzone-file"
-          className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-600 border-dashed rounded-lg cursor-pointer bg-transparent dark:hover:bg-bray-800"
-        >
-          <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg
-              className="w-12 h-12 mb-3 text-red-700"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+        <form>
+          <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-600 border-dashed rounded-lg cursor-pointer bg-transparent dark:hover:bg-bray-800">
+            <div className="flex flex-col items-center justify-center p-5">
+              <CodeMirror
+                value={jsoninput}
+                width="750px"
+                height="230px"
+                theme="dark"
+                extensions={[json(), javascript()]}
+                onChange={onChange}
+              />
+            </div>
+          </label>
+          <div className="flex justify-end gap-4">
+            <button
+              className=" mt-6 text-white border-2 py-2 px-8 focus:outline-none rounded text-lg b"
+              onClick={(e) => {
+                e.preventDefault();
+                setJsoninput("");
+              }}
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-              ></path>
-            </svg>
-            <p className="mb-2 text-sm text-gray-200 dark:text-gray-400">
-              <span className="font-semibold">Select a file</span> or drag and
-              drop
-            </p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">
-              JSON file only size 10MB
-            </p>
+              Clear
+            </button>
+            <button
+              className="mt-6 text-white bg-red-700 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg"
+              onClick={(e) => {
+                e.preventDefault();
+                setJsoninput("");
+              }}
+            >
+              Map
+            </button>
           </div>
-          <input id="dropzone-file" type="file" className="hidden" />
-        </label>
+        </form>
       </div>
     </section>
   );
