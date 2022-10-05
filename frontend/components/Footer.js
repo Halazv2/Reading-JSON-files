d7f200e51ea12cdf8920b8fda93df6ad156f76e3
Mohammed Amine Elazzab <ma.elazzab@gmail.com>
2022-10-05 12:30:16 +0100

    remove button hover

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,41 +1,69 @@
 export default function Footer() {
   return (
     <footer className="pb-4 text-gray-200">
-      <div className="max-w-5xl xl:max-w-5xl mx-auto divide-y divide-gray-900 px-4 sm:px-6 md:px-8">
-        <div className="flex flex-col-reverse justify-between pt-5 pb-4 border-t lg:flex-row bg-top border-black">
-          <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-            <li>
-              <a
-                href="/"
-                className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-              ></a>
-            </li>
-            <li>
-              <a
-                href="/"
-                className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-              >
-                Hala Ziani{" "}
-              </a>
-            </li>
-            <li>
-              <a
-                href="/"
-                className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-              >
-                Kamal Ghrabla
-              </a>
-            </li>
-            <li>
-              <a
-                href="/"
-                className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
-              >
-                Mohamed Amine
-              </a>
-            </li>
-          </ul>
-          <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+      <div class="">
+        <div class="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+          <div class="space-y-12">
+           
+            <ul role="list" class="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-3 lg:max-w-5xl">
+              <li>
+                <div class="space-y-6">
+                  <div className="flex justify-center">
+                  <img
+                    className="object-cover object-center w-3/4 mb-10 rounded-full "
+                    alt="Placeholder Image"
+                    src="https://github.com/AmineElazzab.png"
+                  ></img>
+                  </div>
+                  <div class="space-y-2">
+                    <div class="text-lg leading-6 font-medium space-y-1">
+                      <h3>Mohammed Amine Elazzab</h3>
+                    </div>
+                    
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="space-y-6">
+                  <div className="flex justify-center">
+                  <img
+                    className="object-cover object-center w-3/4 mb-10 rounded-full "
+                    alt="Placeholder Image"
+                    src="https://github.com/Halazv2.png"
+                  ></img>
+                  </div>
+                  <div class="space-y-2">
+                    <div class="text-lg leading-6 font-medium space-y-1">
+                      <h3>Hala Ziani</h3>
+                    </div>
+                 
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="space-y-6">
+                  <div className="flex justify-center">
+                  <img
+                    className="object-cover object-center w-3/4 mb-10 rounded-full "
+                    alt="Placeholder Image"
+                    src="https://github.com/ghrabla.png"
+                  ></img>
+                  </div>
+                  <div class="space-y-2">
+                    <div class="text-lg leading-6 font-medium space-y-1">
+                      <h3>Kamal Rhrabla</h3>
+                    </div>
+                  
+                  </div>
+                </div>
+              </li>
+
+
+            </ul>
+          </div>
+        </div>
+      </div>
+      <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row justify-center">
             <a
               href="/"
               className="text-md text-gray-200 hover:text-white transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold tracking-tight"
@@ -43,8 +71,6 @@ export default function Footer() {
               © 2022 made by YouCodeurs
             </a>
           </ul>
-        </div>
-      </div>
     </footer>
   );
 }
